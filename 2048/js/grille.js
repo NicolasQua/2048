aaac472a81cf1dd995a6ce9010ceca57b6e3c59c
NicolasQua <nicolasquatela.perso@gmail.com>
2023-02-01 14:08:45 +0100

    .

--- 2048/js/grille.js
+++ 2048/js/grille.js
@@ -6,6 +6,7 @@ export default class Grille {
         this.l = l;
         this.initTuiles();
     }
+  
 
 
 
@@ -22,23 +23,26 @@ export default class Grille {
             /* div.addEventListener("click", (e) => {
                 console.log("click tuile");
             }); */ 
-
-            document.onkeydown = (e) => {
-                e = e || window.event;
-                if (e.keyCode === 38) {
-                  console.log('up arrow pressed')
-                } else if (e.keyCode === 40) {
-                  console.log('down arrow pressed')
-                } else if (e.keyCode === 37) {
-                  console.log('left arrow pressed');
-                  this.deplaceGauche();
-                } else if (e.keyCode === 39) {
-                  console.log('right arrow pressed');
-                }
-            }
-            this.updateAffichage();
             console.log("ligne : " + ligne + " colonne : " + colonne + " valeur de la tuile : " + this.tabTuiles[ligne][colonne].val + " index : "+ index);
         }); 
+
+        
+        document.onkeydown = (e) => {
+            e = e || window.event;
+            if (e.keyCode === 38) {
+                console.log('up arrow pressed')
+            } else if (e.keyCode === 40) {
+                console.log('down arrow pressed')
+            } else if (e.keyCode === 37) {
+                console.log('left arrow pressed: tab avant');
+                this.afficheTab();
+                this.deplaceGauche();
+                console.log('tab après');
+                this.afficheTab();
+            } else if (e.keyCode === 39) {
+                console.log('right arrow pressed');
+            }
+        }
     }
 
 
@@ -69,16 +73,38 @@ export default class Grille {
         console.log("deplace gauche");
         for (let l = 0; l < 4; l++) {
             let tuilePress = [];
-            let c =0;
-                while (c < 3) {
-                    if (this.tabTuiles[l][c].val == 0 && this.tabTuiles[l][c+1].val > 0) {
-                        this.tabTuiles[l][c].val = this.tabTuiles[l][c+1].val;
+            let c = 3;
+                while (c != 0) {
+                    if (this.tabTuiles[l][c].val != 0 && this.tabTuiles[l][c-1].val > 0) {
+                        this.tabTuiles[l][c-1].val = this.tabTuiles[l][c].val;
+                        this.tabTuiles[l][c].val = 0;
                     }
                     c++;
                 }
         }
+        this.updateAffichage();
+    }
+
+    updateAffichage() {
+        for (let l = 0; l < 4; l++) {
+            for (let c = 0; c < 4; c++) {
+                let tuile = this.tabTuiles[l][c].val;
+                let index =(l*4) + c;
+                if (tuile != 0) {
+                    document.getElementById(index).innerHTML=tuile.val;
+                } else {
+                    document.getElementById(index).innerHTML="";
+                }
+            }
+
+        }
     }
     
+    afficheTab () {
+        for (let l = 0; l < 4; l++) {
+            console.log(this.tabTuiles[l][0].val + " " +this.tabTuiles[l][1].val + " "+ this.tabTuiles[l][2].val + " " + this.tabTuiles[l][3].val);
+        }
+    }
     create2DArray(rows) {
         let arr = [];
         for (let i = 0; i < rows; i++) {
